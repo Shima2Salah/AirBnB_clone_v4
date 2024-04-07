8be2d9a33ce00fbcdbfa471af06b47309318f577
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-04-07 14:29:28 +0200

    Airbnb

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -6,8 +6,8 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+from uuid import uuid4
 from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -38,7 +38,8 @@ def hbnb():
     return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
+                           places=places,
+                           cache_id=uuid4())
 
 
 if __name__ == "__main__":
